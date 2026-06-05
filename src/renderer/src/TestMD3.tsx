8e1afb4db8292df7452ac5f725bd194ee4aba3ae
PonceGL <poncianogl@hotmail.com>
2026-06-04 21:03:06 -0600

    chore: update @poncegl/material-design-3 to version 0.1.0-alpha.1

--- src/renderer/src/TestMD3.tsx
+++ src/renderer/src/TestMD3.tsx
@@ -2,5 +2,8 @@ import '@poncegl/material-design-3/styles.css'
 import { Button } from '@poncegl/material-design-3'
 
 export function TestMD3(): React.JSX.Element {
-  return <Button>Test MD3 Button</Button>
+  const handleClick = () => {
+    alert('Button clicked')
+  }
+  return <Button onClick={handleClick}>Test MD3 Button</Button>
 }
